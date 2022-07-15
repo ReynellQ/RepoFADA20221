7cddfdb91331d20bb4f43ec57f2442d4a94491c1
Reynell Quevedo <reynell4587@hotmail.com>
2022-07-14 21:00:51 -0500

    fixing in python

--- Python/PuntoA.py
+++ Python/PuntoA.py
@@ -4,75 +4,6 @@ Nombres de los archivos de lectura y escritura, modifique como considere.
 nombreLectura = "inA"
 nombreEscritura = "outA"
 
-
-def input():
-    '''
-    Método para realizar la lectura del problema, no modificar.
-    '''
-    readData = []
-    line = 0
-    def readLine():
-        inputLine = readData[line]
-        line+=1
-        return inputLine
-
-    with open(nombreLectura+".txt", "r") as f:
-        readData = f.read().split('\n')
-        n = int(readLine())
-        p = []
-        for i in range(n):
-            linea = readLine()
-            data = linea.split(" ")
-            horas = data[1].split("-")
-            tiempo = horas[0].split(":")
-            horaI = int(tiempo[0])
-            minI = int(tiempo[1])
-            tiempo = horas[1].split(":")
-            horaF = int(tiempo[0])
-            minF = int(tiempo[1])
-            p.append(Procedimiento(data[0], Hora(horaI, minI), Hora(horaF, minF)))
-        return p
-
-
-
-
-
-def output(obj):
-    '''
-    Método para realizar la escritura de la respuesta del problema, no modificar.
-    '''
-    out = obj.n + "\n"
-    out += obj.tiempoTotal + "\n"
-    for i in range(obj.n):
-        out+= obj.nombreProcedimientos[i] +"\n"
-    
-
-    with open(nombreEscritura+".txt", "w") as f:
-        f.write(out)
-
-
-
-
-def solve(n, procedimientos):
-    """
-    Implementar el algoritmo y devolver un objeto de tipo Respuesta, el cual servirá
-    para imprimir la solución al problema como se requiere en el enunciado.
-    """
-
-    return Procedimiento(0, Hora(0,0), [])
-
-
-def main():
-    p = input()
-    n = len(p)
-    res = solve(n,p)
-    output(res)
-
-
-if __name__ == "__main__":
-    main()
-
-
 class Respuesta():
     def __init__(self, n, tiempoTotal, nombreProcedimientos):
         self.n = n
@@ -90,4 +21,85 @@ class Hora():
         self.hora = hora
         self.minutos = minutos
     def __str__(self):
-        return f'Person name is {self.name} and age is {self.age}'
+        res = ""
+        if (self.hora < 10):
+            res+="0"+str(self.hora)
+        else:
+            res+=str(self.hora)
+        res+=":"
+        if (self.minutos < 10):
+            res+="0"+str(self.minutos)
+        else:
+            res+=str(self.minutos)
+        return res
+
+def input():
+    '''
+    Método para realizar la lectura del problema, no modificar.
+    '''
+    readData = []
+    line = 0
+    def readLine(line):
+        inputLine = readData[line]
+        line+=1
+        return inputLine
+
+    with open(nombreLectura+".txt", "r") as f:
+        readData = f.read().split('\n')
+        n = int(readLine(line))
+        line+=1
+        p = []
+        for i in range(n):
+            linea = readLine(line)
+            data = linea.split(" ")
+            horas = data[1].split("-")
+            tiempo = horas[0].split(":")
+            horaI = int(tiempo[0])
+            minI = int(tiempo[1])
+            tiempo = horas[1].split(":")
+            horaF = int(tiempo[0])
+            minF = int(tiempo[1])
+            p.append(Procedimiento(data[0], Hora(horaI, minI), Hora(horaF, minF)))
+            line+=1
+        return p
+
+
+
+
+
+def output(obj):
+    '''
+    Método para realizar la escritura de la respuesta del problema, no modificar.
+    '''
+    out = str(obj.n) + "\n"
+    out += str(obj.tiempoTotal) + "\n"
+    for i in range(obj.n):
+        out+= obj.nombreProcedimientos[i] +"\n"
+    
+
+    with open(nombreEscritura+".txt", "w") as f:
+        f.write(out)
+
+
+
+
+def solve(n, procedimientos):
+    """
+    Implementar el algoritmo y devolver un objeto de tipo Respuesta, el cual servirá
+    para imprimir la solución al problema como se requiere en el enunciado.
+    """
+
+    return Respuesta(0, Hora(0,0), [])
+
+
+def main():
+    p = input()
+    n = len(p)
+    res = solve(n,p)
+    output(res)
+
+
+if __name__ == "__main__":
+    main()
+
+
--- Python/PuntoB.py
+++ Python/PuntoB.py
@@ -4,67 +4,6 @@ Nombres de los archivos de lectura y escritura, modifique como considere.
 nombreLectura = "inB"
 nombreEscritura = "outB"
 
-
-def input():
-    '''
-    Método para realizar la lectura del problema, no modificar.
-    '''
-    readData = []
-    line = 0
-    def readLine():
-        inputLine = readData[line]
-        line+=1
-        return inputLine
-
-    with open(nombreLectura+".txt", "r") as f:
-        readData = f.read().split('\n')
-        data = readLine().split(" ")
-        n = int(data[0])
-        m = int(data[1])
-        libros = []
-        for i in range(m):
-            data = readLine().split(" ")
-            nombre = data[0]
-            paginas = int(data[1])
-            libros.append(Libro(nombre, paginas))
-        return Entrada(n, m, libros)
-
-
-
-def output(obj):
-    '''
-    Método para realizar la escritura de la respuesta del problema, no modificar.
-    '''
-    out = obj.tiempoTotal + "\n"
-    for i in range(len(obj.libroQueEmpieza)):
-        out+= obj.libroQueEmpieza[i] +" " +obj.libroQueTermina[i] +"\n"
-    
-
-    with open(nombreEscritura+".txt", "w") as f:
-        f.write(out)
-
-
-
-
-
-def solve(n, a, b, ab, ba):
-    """
-    Implementar el algoritmo y devolver un objeto de tipo Respuesta, el cual servirá
-    para imprimir la solución al problema como se requiere en el enunciado.
-    """
-    return Respuesta(0, [],[])
-
-
-def main():
-    n, a, b, ab, ba = input()
-    res = solve(n, a, b, ab, ba)
-    output(res)
-
-
-if __name__ == "__main__":
-    main()
-
-
 class Entrada():
     def __init__(self, n, m, libros):
         self.n = n
@@ -81,3 +20,65 @@ class Libro():
     def __init__(self, nombre, paginas):
         self.nombre = nombre
         self.paginas = paginas
+
+def input():
+    '''
+    Método para realizar la lectura del problema, no modificar.
+    '''
+    readData = []
+    line = 0
+    def readLine(line):
+        inputLine = readData[line]
+        return inputLine
+
+    with open(nombreLectura+".txt", "r") as f:
+        readData = f.read().split('\n')
+        data = readLine(line).split(" ")
+        n = int(data[0])
+        m = int(data[1])
+        line+=1
+        libros = []
+        for i in range(m):
+            data = readLine(line).split(" ")
+            nombre = data[0]
+            paginas = int(data[1])
+            libros.append(Libro(nombre, paginas))
+            line+=1
+        return Entrada(n, m, libros)
+
+
+
+def output(obj):
+    '''
+    Método para realizar la escritura de la respuesta del problema, no modificar.
+    '''
+    out = str(obj.tiempoTotal) + "\n"
+    for i in range(len(obj.libroQueEmpieza)):
+        out+= obj.libroQueEmpieza[i] +" " +obj.libroQueTermina[i] +"\n"
+    
+
+    with open(nombreEscritura+".txt", "w") as f:
+        f.write(out)
+
+
+
+
+
+def solve(n, m, libros):
+    """
+    Implementar el algoritmo y devolver un objeto de tipo Respuesta, el cual servirá
+    para imprimir la solución al problema como se requiere en el enunciado.
+    """
+    return Respuesta(0, [],[])
+
+
+def main():
+    e = input()
+    res = solve(e.n, e.m, e.libros)
+    output(res)
+
+
+if __name__ == "__main__":
+    main()
+
+
